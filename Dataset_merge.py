0b57a03a4f79a388a15f69a767ebaa4b120455c2
zubair0607 <zubair-ali06@hotmail.com>
2025-01-05 18:33:40 +0000

    merged gdp dataset

--- Dataset_merge.py
+++ Dataset_merge.py
@@ -2,15 +2,14 @@ import pandas as pd
 
 pm25_data = pd.read_csv("deaths-from-PM2.5.csv")
 health_data = pd.read_csv("pm25-air-pollution.csv")
+gdp_data = pd.read_csv("air-pollution-gdp.csv")
 
-merged_data = pd.merge(pm25_data, health_data, on=['Entity', 'Year'], how='outer')
-
-print(merged_data.head())
-print(merged_data.columns)
+# merging the datasets
+merged_data = pd.merge(pm25_data, health_data, on=['Country', 'Year'], how='outer')
+merged_data = pd.merge(merged_data, gdp_data, on=['Country', 'Year'], how='outer')
 
 # Renaming the columns
-merged_data.rename(columns={'Entity': 'Country', 
-                            'Absolute deaths from ambient PM2.5 air pollution- State of Global Air': 'Deaths from PM2.5', 
+merged_data.rename(columns={'Absolute deaths from ambient PM2.5 air pollution- State of Global Air': 'Deaths from PM2.5', 
                             'Concentrations of fine particulate matter (PM2.5) - Residence area type: Total': 'PM2.5 levels'}, inplace=True)
 
 # dropping the unnecessary columns
