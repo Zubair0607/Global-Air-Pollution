8ad672b3f273fa40c2d726190375fb39e008d3d0
zubair0607 <zubair-ali06@hotmail.com>
2025-01-05 00:50:26 +0000

    updated dataset merge

--- Dataset_merge.py
+++ Dataset_merge.py
@@ -8,5 +8,13 @@ merged_data = pd.merge(pm25_data, health_data, on=['Year', 'Entity'])
 print(merged_data.head())
 print(merged_data.columns)
 
+# Renaming the columns
+merged_data.rename(columns={'Entity': 'Country', 
+                            'Absolute deaths from ambient PM2.5 air pollution- State of Global Air': 'Deaths from PM2.5', 
+                            'Concentrations of fine particulate matter (PM2.5) - Residence area type: Total': 'PM2.5 levels'}, inplace=True)
+
+# dropping the unnecessary columns
+merged_data = merged_data.drop(columns=["Code_x", "Code_y"])
+
 # Saving the merged data
 merged_data.to_csv("pm25-trends.csv", index=False)
