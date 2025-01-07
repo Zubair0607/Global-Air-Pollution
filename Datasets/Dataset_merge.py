94c7a5fa07562276f3526014b16e1c941e51ef27
zubair0607 <zubair-ali06@hotmail.com>
2025-01-07 18:28:28 +0000

    fixed dataset

--- Datasets/Dataset_merge.py
+++ Datasets/Dataset_merge.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-pm25_data = pd.read_csv("pm25.csv")
-health_data = pd.read_csv("deaths-from-PM2.5.csv")
-gdp_data = pd.read_csv("air-pollution-gdp.csv")
-disease_data = pd.read_csv("pm25-disease-burden.csv")
-emission_data = pd.read_csv("air-pollution-emissions.csv")
+pm25_data = pd.read_csv("Datasets/pm25-air-pollution.csv")
+health_data = pd.read_csv("Datasets/deaths-from-PM2.5.csv")
+gdp_data = pd.read_csv("Datasets/air-pollution-gdp.csv")
+disease_data = pd.read_csv("Datasets/pm25-disease-burden.csv")
+emission_data = pd.read_csv("Datasets/air-pollution-emissions.csv")
 
 # merging the datasets
 merged_data = pd.merge(pm25_data, health_data, on=['Country', 'Year'], how='outer')
@@ -12,11 +12,12 @@ merged_data = pd.merge(merged_data, gdp_data, on=['Country', 'Year'], how='outer
 merged_data = pd.merge(merged_data, disease_data, on=['Country', 'Year'], how='outer')
 merged_data = pd.merge(merged_data, emission_data, on=['Country', 'Year'], how='outer')
 
-
+# Renaming the columns
+merged_data.rename(columns={'Absolute deaths from ambient PM2.5 air pollution- State of Global Air': 'Deaths from PM2.5', }, inplace=True)
 
 # dropping the unnecessary columns and rows
-merged_data = merged_data.drop(columns=["Code_x", "Code_y"])
-merged_data = merged_data.dropna(subset=['Deaths from PM2.5', 'PM2.5 levels','GDP per Capita'], how='all')
+merged_data = merged_data.drop(columns="Code")
+merged_data = merged_data.dropna(subset=['Deaths from PM2.5', 'PM2.5 Levels','GDP per Capita'], how='all')
 
 # Saving the merged data
-merged_data.to_csv("pm25-trends.csv", index=False)
+merged_data.to_csv("Datasets/MAIN_pm25-trends.csv", index=False)
